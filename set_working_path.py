cbfba6dea90df37256b2b53062a8cd7110b20c20
dmytro <dmytro@dmytro-LIFEBOOK-SH531.(none)>
2012-11-10 17:25:11 +0200

    add root path

--- set_working_path.py
+++ set_working_path.py
@@ -3,3 +3,8 @@
 """
 This file contains paths for working files.
 """
+# Set root path to all working files.
+win_root_path = 'd:\\Programming\\Python_Projects\\'
+lin_root_path = '/home/dmytro/Knowledge/Programming/Python_Projects'
+
+# Set path to base_data.csv file.
