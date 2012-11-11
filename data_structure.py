fcbeea3935035277a4606fd03f8893e93038fca5
dmytro <dmytro@dmytro-LIFEBOOK-SH531.(none)>
2012-11-11 19:59:00 +0200

    read base_data.csv file

--- data_structure.py
+++ data_structure.py
@@ -3,3 +3,24 @@
 """
 The script is purposed for data structure composition from base csv-file.
 """
+import csv
+from soft4recieve_write_Rel2.set_working_path import base_data
+
+# Import data from core csv file for creating file structure
+bd_csv = csv.reader(base_data,delimiter=';')
+
+out_list, size_list, shift_list = [],[],[]
+type_list, long_name, cut_name, = [],[],[]
+val_list, group_list, BOM_list = [],[],[]
+
+for column in bd_csv:
+    out_list.append(int(column[0]))
+    size_list.append(int(column[1]))
+    shift_list.append(int(column[2]))
+    type_list.append(column[3])
+    long_name.append(column[4])
+    cut_name.append(column[5])
+    val_list.append(int(column[6]))
+    group_list.append(column[7])
+    BOM_list.append(column[8])
+print BOM_list
