4451849ba98777b863ca4bd4ba18f09919fc2fc0
dmytro <dmytro@dmytro-LIFEBOOK-SH531.(none)>
2012-11-11 13:42:36 +0200

    update create_path fun and add path_valid fun

--- main_lib.py
+++ main_lib.py
@@ -18,19 +18,30 @@ def create_path(os_dict):
     """
     for os_key in os_dict:
         if sys.platform.startswith(os_key):
-            print 'Your path:',
             return os_dict[os_key]
 
-
+def path_valid(dir_path):
+    """
+    Function validates working path from incoming dir_path dictionary and
+    returns out_path dictionary this validated paths for correct path.
+    """
+    out_dict = copy.deepcopy(dir_path)
+    for p in dir_path:
+        if not os.path.exists(dir_path[p]):
+            out_dict[p] = 'Your *%s* path is invalid!' % p
+    return  out_dict
 
 
 if __name__ == "__main__":
     dic_path = {'win32':{'in':'in path for win','out':'out path for win'},
-                'linux2':{'in':'in path1 for lin','out1':'/home/dmytro',
-                          'out2':'out path2 for lin'},
+                'linux2':{'in':'in path1 for lin','out1':'/home/dmytro/',
+                          'out2':'/home/dmytroKnowledge/Programming/Python_Projects/soft4recieve_write_Rel2/'},
                 'mac':{'in':'in path for mac','out':'out path for mac'}}
     dic_path2 = {'win32':'none','linux2':'/home/dmytro'}
-    print create_path(dic_path)
+    print 'From create_path: ', create_path(dic_path)
+    print
+    dic_path3 = create_path(dic_path)
+    print 'Your path: %s' % path_valid(dic_path3)
 #    dic_path3 = copy.deepcopy(dic_path)
 #    print dic_path3
 #    del dic_path3['win32']['in']
