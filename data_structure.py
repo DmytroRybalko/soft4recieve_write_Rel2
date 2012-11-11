8711886e6f3550a4cda3cdd47c2bf6c764f50558
dmytro <dmytro@dmytro-LIFEBOOK-SH531.(none)>
2012-11-11 22:04:04 +0200

    update base_data.cvs and structure_data.py

--- data_structure.py
+++ data_structure.py
@@ -9,30 +9,29 @@ from soft4recieve_write_Rel2.set_working_path import base_data
 # Import data from core csv file for creating file structure
 bd_csv = csv.reader(base_data,delimiter=';')
 
-out_list, size_list, shift_list = [],[],[]
+size_list, shift_list = [],[]
 type_list, long_name, cut_name, = [],[],[]
 val_list, group_list, BOM_list = [],[],[]
 
 for column in bd_csv:
-    out_list.append(int(column[0]))
-    size_list.append(int(column[1]))
-    shift_list.append(int(column[2]))
-    type_list.append(column[3])
-    long_name.append(column[4])
-    cut_name.append(column[5])
-    val_list.append(int(column[6]))
-    group_list.append(column[7])
-    BOM_list.append(column[8])
+    size_list.append(int(column[0]))
+    shift_list.append(int(column[1]))
+    type_list.append(column[2])
+    long_name.append(column[3])
+    cut_name.append(column[4])
+    val_list.append(int(column[5]))
+    group_list.append(column[6])
+    BOM_list.append(column[7])
+long_name = [i.decode('cp1251') for i in long_name]
 
 # Create data store structure
 buf_list = []
-for pos in range(len(out_list)):
+for pos in range(len(size_list)):
     buf_dic = {}
-    buf_dic['out'] = out_list[pos]
     buf_dic['size'] = size_list[pos]
     buf_dic['shift'] = shift_list[pos]
     buf_dic['type'] = type_list[pos]
-    buf_dic['name'] = long_name[pos].decode('cp1251')
+    buf_dic['name'] = long_name[pos]
     buf_dic['cut_name'] = cut_name[pos]
     buf_dic['val'] = val_list[pos]
     buf_dic['group'] = group_list[pos]
@@ -42,8 +41,24 @@ for pos in range(len(out_list)):
 packets = tuple(buf_list)
 
 if __name__ == "__main__":
-#    print BOM_list
-    print packets[3]
+    print 'size_list: ', size_list
+    print '======================='
+    print 'shift_list', shift_list
+    print '======================='
+    print 'type_list', type_list
+    print '======================='
+    print 'long_name: '
+    for i in long_name:
+        print i
+    print '======================='
+    print 'cut_name', cut_name
+    print '======================='
+    print 'val_list', val_list
+    print '======================='
+    print 'group_list', group_list
+    print '======================='
+    print 'BOM_list', BOM_list
+    print packets[2]
     print packets[95]['cut_name']
     print
     print [packets.index(pack) for pack in packets if pack['group'] == 'IMU']
