bb6aa91ff2af48d17c59729b99fa0d96ab3ed210
dmytro <dmytro@dmytro-LIFEBOOK-SH531.(none)>
2012-11-16 12:26:13 +0200

    add sort_files fun

--- main_lib.py
+++ main_lib.py
@@ -45,3 +45,28 @@ def get_path4file(os_dict,p_key,file_name,file_mode):
         return open(work_path[p_key] + file_name, file_mode)
     except:
         print 'Something wrong this your path!'
+
+def sort_files(file_list,sep=''):
+    # функция выполняет сортировку списка имен файлов, содержащих числовые значения
+    # file_list - список имен файлов
+    # Считываем данные из файла
+    key,val = [],[]
+    if sep != '':
+        FL = []
+        for fname in file_list:
+            fn4d = fname.split('\\')[-1][:(-4)] #Разбиваем строку по разделителю "\\", выделяем последний элемент списка и удаляем последние 4 символа:'
+            if len(fn4d.split(sep)) > 1:        # if 'sep' in file name - add it to list
+                FL.append(fname)
+    else:
+        FL = file_list[:]
+    for fname in FL:
+        fn4d = fname.split('\\')[-1][:(-4)] #Разбиваем строку по разделителю "\\", выделяем последний элемент списка и удаляем последние 4 символа:'
+        buf1 = fn4d.split('-').pop(-1)# choose string that conteins hh_mm
+        buf2 = buf1.split('.')        # split string into two numbers
+        part1 = int(buf2[0])*100
+        part2 = int(buf2[1])
+        number = part1 + part2        # form complex number with hours and minutes
+        key.append(int(number))
+        val.append(fname)
+        D = dict(zip(key,val)) # Формируем словарь вида {num:'file name'}
+    return D
