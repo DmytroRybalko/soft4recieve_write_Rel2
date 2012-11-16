09468f2d69201060e5544410265f02670227237f
dmytro <dmytro@dmytro-LIFEBOOK-SH531.(none)>
2012-11-16 10:19:58 +0200

    add converter.py and lib4converter.py

---
+++ Converter/converter.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""
+This script converts data from files which have been created by moSINS software or
+test4converter.py script into text file. Indeed script provides to user choice
+which data he or she can convert. Converted data save into text file.
+"""
---
+++ Converter/lib4converter.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+"""
+This script contains a set of auxilliary functions for converter.py and
+test4converter.py scripts.
+"""
