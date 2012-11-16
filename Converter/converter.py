727740f5352c90eafb75e4eff1fa37ef3a517358
dmytro <dmytro@dmytro-LIFEBOOK-SH531.(none)>
2012-11-16 13:04:43 +0200

    add wrap4read_files fun

--- Converter/converter.py
+++ Converter/converter.py
@@ -5,3 +5,35 @@ This script converts data from files which have been created by moSINS software 
 test4converter.py script into text file. Indeed script provides to user choice
 which data he or she can convert. Converted data save into text file.
 """
+
+import glob
+from set_working_path import conv_data
+from main_lib import *
+
+#==============================================================================
+# First we have to find out which data source groups are in binary file. For
+# this we read first line from first binary file, search substrings which
+# contain symbol '#' (h23) snd exclude groups from all_data dictionary which
+# contain this symbol.
+
+#==============================================================================
+# Create list of files for converting
+file_list = glob.glob(get_path(conv_data)['in1'] + '*.dat')
+# Create dictionary of binary files for converting
+file_dict = sort_files(file_list,'bin')
+#==============================================================================
+# Choose first file from file_dict
+first_count = 0
+for ord_num in sorted(file_dict):
+    if first_count == 0:
+        first_file = open(file_dict[ord_num],'r')
+    else:
+        break
+#==============================================================================
+# Read first line from first binary file to analyse it
+first_line = first_file.read(1100)# My propositions: set this number as constant
+first_file.close()
+
+print get_path(conv_data)['in1']
+print
+print first_line
--- Converter/lib4converter.py
+++ Converter/lib4converter.py
@@ -4,3 +4,48 @@
 This script contains a set of auxiliary functions for converter.py and
 test4converter.py scripts.
 """
+
+import glob
+from set_working_path import conv_data
+from main_lib import *
+
+def wrap4read_files():
+    """
+
+    """
+    # Create list of files for converting
+    file_list = glob.glob(get_path(conv_data)['in1'] + '*.dat')
+    # Create dictionary of binary files for converting
+    file_dict = sort_files(file_list,'bin')
+    row_count = 0
+    # Choose first file from file_dict
+    for ord_num in sorted(file_dict):
+        for ord_num in sorted(file_dict):# Open binary files for reading
+            raw_file = open(file_dict[ord_num],'r')
+            for line in raw_file:
+                row_count += 1
+                pass
+                # Some function
+            raw_file.close()
+            pass
+        pass
+
+
+#==============================================================================
+# Read first line from first binary file to analyse it
+first_line = first_file.read(1100)# My propositions: set this number as constant
+first_file.close()
+
+#==============================================================================
+# Choose first file from file_dict
+first_count = 0
+for ord_num in sorted(file_dict):
+    if first_count == 0:
+        first_file = open(file_dict[ord_num],'r')
+    else:
+        break
+#==============================================================================
+# Read first line from first binary file to analyse it
+first_line = first_file.read(1100)# My propositions: set this number as constant
+first_file.close()
+print first_line
