dbcbd1a93e6722cb3dd79b418e2fce7c106d3349
dmytro <dmytro@dmytro-LIFEBOOK-SH531.(none)>
2012-11-17 22:15:29 +0200

    add test for get_first_line and get_available_data fun

---
+++ Converter/test/Converter/get_available_data/test_converter.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+This file contains unittests for converter.py.
+"""
+import unittest
+import itertools
+from Converter.converter import get_available_data
+
+if __name__ == "__main__":
+    pass
