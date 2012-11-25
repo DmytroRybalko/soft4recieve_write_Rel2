085b4d23b2aa580f0a899bf27568805c55807b41
dmytro <dmytro@dmytro-LIFEBOOK-SH531.(none)>
2012-11-25 15:52:51 +0200

    add extract_kkp_frame_cell fun

--- moSINS/lib4kkp/lib4kkp.py
+++ moSINS/lib4kkp/lib4kkp.py
@@ -3,3 +3,21 @@
 """
 This script contains a set of auxiliary functions for mapping KKP data.
 """
+
+def extract_kkp_frame_cell(fr_cel):
+    """
+    Function extract frame's snd cell's values.
+
+    Keyword arguments:
+    fr_cel -- hex string
+    """
+
+    if fr_cel > 0 & fr_cel < 205:
+        cell = fr_cel%8
+        frame = fr_cel/8
+    elif fr_cel == 0:
+        cell = 0
+        frame = 0
+    else:
+        frame = 1
+    return (frame, cell)
