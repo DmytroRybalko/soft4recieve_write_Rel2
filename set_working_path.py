2803f2dcc3446cd27f9ee7169edca669f2b6413e
Dmytro <mail4dmytro@gmail.com>
2013-01-31 13:13:30 +0200

    add test_path to set_working_path

--- set_working_path.py
+++ set_working_path.py
@@ -20,5 +20,9 @@ conv_data = {'win32':{'in1':win_root_path +'Converter\\BIN_files',
              'linux2':{'in1':lin_root_path +'/Converter/BIN_files/',
                        'out1':lin_root_path +'/Converter/CONVERTED_files'}}
 
+# Set path for testing functions
+test_data = {'win32':{'get_first_line':win_root_path +
+                                       'Converter\\test\\Converter\\get_first_line\\'}}
+
 # Set format style for output text files
 data_style = {'f_style':'.8f','i_style':'d','l_style':'l','s_style':'s'}
