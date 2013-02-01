bca031ea5137119f6045f85563d13ea634c2c0b6
Dmytro <mail4dmytro@gmail.com>
2013-02-01 10:10:35 +0200

    improve present of result of test_get_available_data fun

--- Converter/test/Converter/get_available_data/test_get_available_data.py
+++ Converter/test/Converter/get_available_data/test_get_available_data.py
@@ -11,12 +11,10 @@ from soft4recieve_write_Rel2.Converter.converter import get_available_data
 
 if __name__ == "__main__":
     # Create list of files for converting
-    path = ('d:\\Programming\\Python_Projects\\soft4recieve_write_Rel2\\' +
-                  'Converter\\Converter\\BIN_files\\')
-    # Create dictionary of binary files for converting
     file_list = glob.glob(get_path(test_data)['common'] + '*.dat')
-#    file_list = glob.glob(path + '*.dat')
+    # Create dictionary of binary files for converting
     file_dict = sort_files(file_list,'bin')
-#    print file_list
+    print 'List of files: ', file_list
     result = get_available_data(file_dict)
-    print 'Available data: ', result
+    print '==============================='
+    print 'Available data: ',set([i['group'] for i in result if i.has_key('group')])
