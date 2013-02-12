21b7720ffc3ac3abe4067a7c73cb61c9fb593c6e
Dmytro <mail4dmytro@gmail.com>
2013-02-12 16:21:37 +0200

    add new functions test4,test5,test6

--- Converter/test/Converter/main_fun/test_main_fun.py
+++ Converter/test/Converter/main_fun/test_main_fun.py
@@ -11,16 +11,25 @@ from soft4recieve_write_Rel2.Converter.converter import (get_available_data,
     main_fun, sort_file_dict, date_from_bin_file, line_from_file)
 
 def test1(x):
-    string = '1'
-    return "Give me arg1 %s" % x
+    return "Give me arg1 %s" % str(x)
+
+def test4(**args):
+    return "Give me arg1 %s" % args['arg1']
 
 def test2(x,y):
-    string = '2'
     return "Give me arg1 %s, agr2 %s" % (str(x),str(y))
 
+def test5(**args):
+    return "Give me arg1 %s, agr2 %s" % (args['arg1'],args['arg2'])
+
 def test3(x,y,z):
-    string = '3'
     return "Give me arg1 %s, arg2 %s, arg3 %s" % (str(x),str(y),str(z))
+
+def test6(**args):
+    return "Give me arg1 %s, arg2 %s, arg3 %s" % (args['arg1'],args['arg2'],args['arg3'])
+
+def test4(**kargs):
+    print "Give me arg1 %s, arg2 %s, arg3 %s" % (kargs['arg1'],kargs['arg2'],kargs['arg3'])
 
 def fabric_fun(fun,**kvargs):
     return 'Result of %s '%fun.__name__, fun(kvargs)
@@ -48,18 +57,36 @@ def main2():
             print 'I am %s'%fun.__name__
             print fun(*func_args[fun.__name__]),'\n'
 
+def main3(func_pool,args):
+    """
+
+    """
+    for fun in func_pool:
+        print 'I am %s'%fun.__name__
+        print fun(**args),'\n'
+
 if __name__ == "__main__":
     print 'Test main1 func\n=================='
     # Create pool of functions
     func_pool = (test1,test2,test3)
     # Create dict that connects function's name and tuple of arguments
     func_args = {'test1':'a','test2':('b','c'),'test3':('b','a','c')}
-    main1(func_pool,func_args)
+#    main1(func_pool,func_args)
     print '==================\n'
 
     print 'Test main2 func\n=================='
-    main2()
+#    main2()
     print '==================\n'
+
+    # Create dict of named arguments
+    func_pool2 = (test4,test5,test6)
+    kargs = {'arg1':'1','arg2':'2','arg3':'3'}
+    print test4(**kargs)
+    for fun in func_pool2:
+        print 'I am %s'%fun.__name__
+        print fun(**kargs),'\n'
+#    main3(func_pool,kargs)
+
 
     # Get dictionary of binary files
     sfd = sort_file_dict(test_path['common'])
@@ -80,6 +107,8 @@ if __name__ == "__main__":
 #    print 'Attribute test'
 #    test1('3')
 #    print  dir(test1)
+#    print test1.__dict __
+#    help(test1.func_dict)
 #    test1.my_file_name = 'test.dat'
 #    test1.__setattr__('my_file_name','test\\test.dat')
 #    print dir(test1)
