a5fe1f5c6624700392721d403607ea8bbfea3c22
Dmytro <mail4dmytro@gmail.com>
2013-02-12 11:41:23 +0200

    add and tested main1 and main2 functions in test_main_file

---
+++ Converter/test/Converter/main_fun/test_main_fun.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+"""
+This file contains unittests for converter.py.
+"""
+import unittest
+import glob
+from soft4recieve_write_Rel2.main_lib import user_group_name2str
+from soft4recieve_write_Rel2.set_working_path import fun_path, test_path
+from soft4recieve_write_Rel2.Converter.converter import (get_available_data,
+    main_fun, sort_file_dict, date_from_bin_file)
+
+def test1(x):
+    string = '1'
+    return "Give me arg1 %s" % x
+
+def test2(x,y):
+    string = '2'
+    return "Give me arg1 %s, agr2 %s" % (x,y)
+
+def test3(x,y,z):
+    string = '3'
+    return "Give me arg1 %s, arg2 %s, arg3 %s" % (x,y,z)
+
+def fabric_fun(fun,**kvargs):
+    return 'Result of %s '%fun.__name__, fun(kvargs)
+
+def main1(func_pool,func_args):
+    """
+    This is first part of main function which demostrade pathing tupels of
+    arguments into dictionary of functions.
+    """
+    for fun, arg in zip(func_pool,func_args):
+        print '%s, I am %s'%(fun(*func_args[fun.__name__]), fun.__name__)
+
+def main2():
+    """
+    This is the second part of main function which separate tupels of arguments
+    for functions according fuctions.
+
+    vargs -- list of tuples with arguments
+    """
+    tup1 = ('1','2','3')
+    tup2 = ('10','20','30')
+    tup3 = ('100','200','300')
+    for a, b, c in zip(tup1,tup2,tup3):
+        func_args = {'test1':a,'test2':(b,c),'test3':(c,b,a)}
+        for fun, arg in zip(func_pool,func_args):
+            print '%s, I am %s'%(fun(*func_args[fun.__name__]), fun.__name__)
+
+if __name__ == "__main__":
+    print 'Test main1 func\n=================='
+#    print '==================\n'
+    # Create pool of functions
+    func_pool = (test1,test2,test3)
+    # Create dict that connects function's name and tuple of arguments
+    func_args = {'test1':'a','test2':('b','c'),'test3':('b','a','c')}
+    main1(func_pool,func_args)
+    print '==================\n'
+    print 'Test main2 func\n=================='
+    main2()
+    print '==================\n'
+
+
+    # Get dictionary of binary files
+    sfd = sort_file_dict(test_path['common'])
+    # Get full first file's name
+    fff_name = sfd[sorted(sfd)[0]]
+    # Get list of available data
+    new_packets = get_available_data(sfd)
+    # Get group's names of available data
+    group_data = user_group_name2str(new_packets)
+    print 'Group\'s names of available data: \n', group_data
+    local_path = fun_path['main_fun']
+    path = local_path(group_data,date_from_bin_file(fff_name))
+
+
+    for fun, arg in zip(func_pool,func_args):
+        print fun(*func_args[fun.__name__])
+
+    print "=================================================="
+#    print (test3.__code__.co_varnames)
+#    print 'Attribute test'
+    test1('3')
+#    print  dir(test1)
+#    test1.my_file_name = 'test.dat'
+#    test1.__setattr__('my_file_name','test\\test.dat')
+#    print dir(test1)
+#    print test1.my_file_name
+
+
