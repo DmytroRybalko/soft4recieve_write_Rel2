51ef73def966c92159fe321ac21cbd3891fa4ca0
Dmytro <mail4dmytro@gmail.com>
2013-02-14 16:11:53 +0200

    create main.py file

---
+++ Converter/main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+This module get informaton from converter.py about what and how data have to be
+converted by calling nesessery functions from mapping.py modules. Result is
+written into files.
+"""
+def main():
+    """
+
+    """
+    pass
