21a8e5a64a677ae3515638075dd3d175f437015a
Dmytro <mail4dmytro@gmail.com>
2013-02-16 12:39:49 +0200

    update path to bin files in test_get_available_data and test_get_user_data

--- Converter/test/Converter/get_available_data/test_get_available_data.py
+++ Converter/test/Converter/get_available_data/test_get_available_data.py
@@ -4,17 +4,12 @@
 This file contains unittests for get_available_data function in converter.py
 """
 import unittest
-import glob
-from soft4recieve_write_Rel2.main_lib import get_path,sort_files
-from soft4recieve_write_Rel2.set_working_path import test_data
-from soft4recieve_write_Rel2.Converter.converter import get_available_data
+from soft4recieve_write_Rel2.set_working_path import test_path
+from soft4recieve_write_Rel2.Converter.converter import get_available_data, sort_file_dict
 
 if __name__ == "__main__":
-    # Create list of files for converting
-    file_list = glob.glob(get_path(test_data)['common'] + '*.dat')
     # Create dictionary of binary files for converting
-    file_dict = sort_files(file_list,'bin')
-    print 'List of files: ', file_list
+    file_dict = sort_file_dict(test_path['common'])
     result = get_available_data(file_dict)
     print '==============================='
     print 'Available data: ',set([i['group'] for i in result if i.has_key('group')])
--- Converter/test/Converter/get_user_data/test_get_user_data.py
+++ Converter/test/Converter/get_user_data/test_get_user_data.py
@@ -4,18 +4,17 @@
 This file contains unittests for converter.py.
 """
 import unittest
-import glob
-from soft4recieve_write_Rel2.main_lib import get_path,sort_files
-from soft4recieve_write_Rel2.set_working_path import test_data
+from soft4recieve_write_Rel2.set_working_path import test_path
 from soft4recieve_write_Rel2.Converter.converter import get_available_data
+from soft4recieve_write_Rel2.Converter.converter import sort_file_dict
 from soft4recieve_write_Rel2.Converter.converter import show_available_data
 from soft4recieve_write_Rel2.Converter.converter import get_user_data
 
 if __name__ == "__main__":
     # Create list of files for converting
-    file_list = glob.glob(get_path(test_data)['common'] + '*.dat')
+#    file_list = glob.glob(get_path(test_data)['common'] + '*.dat')
     # Create dictionary of binary files for converting
-    file_dict = sort_files(file_list,'bin')
+    file_dict = sort_file_dict(test_path['common'])
     # Get list of available data
     new_packets = get_available_data(file_dict)
     # Show list of available data
